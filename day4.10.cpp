d4e939833f1e2eae3f8e3aa3de84f5e814a67781
Brien-GGG <86729482+Brien-GGG@users.noreply.github.com>
2021-07-01 18:51:29 +0400

    Update day4.10.cpp

--- day4.10.cpp
+++ day4.10.cpp
@@ -7,6 +7,8 @@ main ()
   int arrsize;
   std::cin >> arrsize;
   int arr1[arrsize][arrsize] = { 0 };
+  int mainofarr[arrsize]={0};
+  int sideofarr[arrsize]={0};
 
   std::cout << " Enter the values of matrix by rows:";
   for (int r = 0; r < arrsize; ++r)
@@ -22,28 +24,25 @@ main ()
 
   std::cout << std::endl << "the main diagonal values is: ";
   for (int r = 0, k = 0; r < arrsize; ++r, ++k)
+  {
     std::cout << arr1[r][k] << " ";
+    mainofarr[r]=arr1[r][k];
+  }
 
   std::cout << std::endl << "the side diagonal values is: ";
   for (int r = arrsize - 1, k = 0; r >= 0, k < arrsize; --r, ++k)
+  {
     std::cout << arr1[r][k] << " ";
-
-
-  int tmp1 = 0;
-  int tmp2 = 0;
-  for (int r = 0, k = 0; r < arrsize; ++r, ++k)
-    {
-      tmp1 = arr1[r][k];
-      for (int r=arrsize-1,k=0; r>=arrsize, k<arrsize; --r,++k)
-      {
-          tmp2 = arr1[r][k];
-          arr1[r][k] =
-      }
-      arr1[r][k] = tmp2;
-    }
-
-
-
+    sideofarr[k]=arr1[r][k];
+  }
+  
+  std::cout <<std::endl <<" After swaping main & side diagnals" <<std::endl;
+  
+  for(int r=0, k=0; r<arrsize; ++r,++k)
+  arr1[r][k]=sideofarr[r];
+  for(int r=arrsize-1, k=0; r>=0, k<arrsize; --r,++k)
+  arr1[r][k]=mainofarr[k];
+  
   for (int r = 0; r < arrsize; ++r)
     {
       std::cout << std::endl;
